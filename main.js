0e709412a431b574d88f770bbddeeb93175faa05
elho1904 <eline@holthe.org>
2024-03-22 11:26:41 +0100

    legger til drop-down meny i navbar

--- main.js
+++ main.js
@@ -13,3 +13,8 @@ function toggleImage() {
     var bilde = document.getElementById('nettvett-img');
     bilde.classList.toggle('visible');
 }
+
+function menu() {
+    const linker = document.getElementById("linker")
+    linker.classList.toggle("show")
+}
