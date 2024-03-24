adbbd133f47fd9417d70628abc5b93d6bd40a7a4
satyve <152157314+satyve@users.noreply.github.com>
2024-03-24 14:10:26 +0100

    små endringer

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// function showList() {
-//     let infoDiv = document.querySelector('.sourceInnhold')
-//     let arrowIcon = document.getElementById('arrowIcon')
-
-//     if (infoDiv.style.display === 'none' || infoDiv.style.display === '') {
-//         infoDiv.style.display = 'flex'
-//         arrowIcon.classList.add('rotate180') // Legg til klassen for å rotere ikonet
-//     } else {
-//         infoDiv.style.display = 'none'
-//         arrowIcon.classList.remove('rotate180') // Fjern klassen for å rotere ikonet
-//     }
-// }
 
 function showList(){
     let infoDiv = document.querySelector('.sourceInnhold')
