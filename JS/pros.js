6c6c3bcfa41cfdd68b0fd880ab8b6ca57f1f2cf7
Betaniyad <betaniyadejene@gmail.com>
2024-03-24 21:36:18 +0100

    endringer i nettvettreglene i pros

--- JS/pros.js
+++ JS/pros.js
@@ -1,9 +1,21 @@
+//function toggleImage() {
+   // var bilde = document.getElementById('nettvett-img');
+    //bilde.classList.toggle('visible');
+//}
+
+//function menu() {
+  //  const linker = document.getElementById("linker")
+    //linker.classList.toggle("show")
+//}
+
 function toggleImage() {
     var bilde = document.getElementById('nettvett-img');
     bilde.classList.toggle('visible');
-}
-
-function menu() {
-    const linker = document.getElementById("linker")
-    linker.classList.toggle("show")
+    if (bilde.classList.contains('visible')) {
+        bilde.style.display = 'block'; // Viser bildet når det blir synlig
+    } else {
+        setTimeout(function() {
+            bilde.style.display = 'none'; // Skjuler bildet etter at overgangen er fullført
+        }, 500); // Endre dette tilsvarende overgangstiden i millisekunder
+    }
 }
