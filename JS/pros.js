1e42bd9d8ca35f936bd28fbc7bc0a3ef21ab0de8
Betaniyad <betaniyadejene@gmail.com>
2024-03-24 21:43:14 +0100

    små endringer i pros css

--- JS/pros.js
+++ JS/pros.js
@@ -1,21 +1,11 @@
-//function toggleImage() {
-   // var bilde = document.getElementById('nettvett-img');
-    //bilde.classList.toggle('visible');
-//}
-
-//function menu() {
-  //  const linker = document.getElementById("linker")
-    //linker.classList.toggle("show")
-//}
-
 function toggleImage() {
     var bilde = document.getElementById('nettvett-img');
     bilde.classList.toggle('visible');
     if (bilde.classList.contains('visible')) {
-        bilde.style.display = 'block'; // Viser bildet når det blir synlig
+        bilde.style.display = 'block'; // Viser bildet når det er synlig
     } else {
         setTimeout(function() {
-            bilde.style.display = 'none'; // Skjuler bildet etter at overgangen er fullført
-        }, 500); // Endre dette tilsvarende overgangstiden i millisekunder
+            bilde.style.display = 'none'; // Skjuler bildet når overgangen slutter
+        }, 500); // Overgangstiden
     }
 }
