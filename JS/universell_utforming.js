0098c0fd5b4292dd62e565adf35aff52e8bfc716
lunarctic <158510510+lunarctic@users.noreply.github.com>
2024-03-24 16:45:33 +0100

    noen små endringer

--- JS/universell_utforming.js
+++ JS/universell_utforming.js
@@ -1,14 +1,14 @@
 const accordion = document.getElementById("accordion");
-        const arrow = document.querySelector("#arrowIcon2")
-            accordion.addEventListener("click", function(){
-                accordion.classList.toggle("active");
-                const panel = document.getElementById("panel");
-                if (panel.style.maxHeight) {
-                    panel.style.maxHeight = null;
-                    arrow.classList.remove('rotate180')
-                    arrow.style.transition= '1s ease' 
-                } else {
-                    panel.style.maxHeight = panel.scrollHeight + "px";
-                    arrow.classList.add('rotate180')
-                }
-            });
+const arrow = document.querySelector("#arrowIcon2")
+accordion.addEventListener("click", function () {
+    accordion.classList.toggle("active");
+    const panel = document.getElementById("panel");
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        arrow.classList.remove('rotate180')
+        arrow.style.transition = '1s ease'
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        arrow.classList.add('rotate180')
+    }
+});
